4dad0192f29c76b44dc4c6740531f400cbce7c00
leonr7600-create <leonr7600@gmail.com>
2026-01-27 12:11:52 -0600

    Update love meter and celebration messages

--- config.js
+++ config.js
@@ -42,16 +42,16 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "WOOOOW You love me that much?? 🚀💝",  // Shows when they go past 5000%
+        high: "Goddamn! 🚀💝",              // Shows when they go past 1000%
+        normal: "And beyond! ❤️❤️"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest man in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        message: "We will have a special date with great food. Save the date in case Francheska wants to do something else!",
+        emojis: "🎁💖🤗💝❤️"  // These will bounce around
     },
 
     // Color scheme for the website
