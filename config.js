b2ce8c0010f491c3309244687d1891a99998fd6e
leonr7600-create <leonr7600@gmail.com>
2026-03-09 16:21:59 -0600

    Increase volume level to 1.0 in config.js

--- config.js
+++ config.js
@@ -81,7 +81,7 @@ const CONFIG = {
         musicUrl: "https://res.cloudinary.com/dzvqg4vox/video/upload/v1773094626/Pat_Benatar_-_We_Belong_Official_Music_Video_hl6vsn.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+        volume: 1.0                        // Volume level (0.0 to 1.0)
     }
 };
 
